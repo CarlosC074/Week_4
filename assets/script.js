27ce82f0e482a0d3cadc7a6d26ab9464d8dd66b7
CarlosC074 <carlos.castano74@gmail.com>
2021-09-19 23:02:21 -0700

    Quiz is held by radio inputs. answers are processed and decideswhether to increment the point count up or not based on user input. Points are currently logged in the browser's console.

--- assets/script.js
+++ assets/script.js
@@ -34,35 +34,65 @@ var questions = [
   ];
   
     var qindex = 0
-    
+    //counts how many correct answers there are
+    var points = 0
+  
+  
+
+  function checkAnswer(index) {
+    var realIndex = index-1;
+    var myForm = document.getElementsByName(`quizQuestion_${realIndex}`);
+    const qAnswer = questions[realIndex].answer;
+
+    for(var i = 0; i < myForm.length; i++) {
+      const choice = myForm[i].checked
+      const input = myForm[i].value;
+
+      
+      if(input === qAnswer && choice === true) {
+        points += 1;
+      }
+    }
+    console.log(points);
+  }
+
   function nextQuestion(index) {
-     document.querySelector('.quiz').classList.toggle('hidden')
+    document.querySelector('.answer').classList.remove('hiddenAnswer')
+    setTimeout( function() { document.querySelector('.quiz').classList.toggle('hidden')
     if(index < questions.length) {    
       
       var randoQ = questions[index]
   
       var title = document.querySelector('.title')
-      var choices = document.querySelector('.choices ul')
+      var choices = document.querySelector('.form')
       var answer = document.querySelector('.answer')
-  
+      answer.classList.add('hiddenAnswer');
+
       title.textContent = randoQ.title
-      choices.innerHTML = randoQ.choices.map(choice => `<li>${choice}</li>`).join('')
+      choices.innerHTML = randoQ.choices.map(choice => `<input type= "radio" id= "${choice}" name= "quizQuestion_${index}" value= "${choice}" required>
+      <label for= "${choice}">${choice}</label>`).join('<br>')
       answer.textContent = randoQ.answer
-      
+
+    
+
       qindex++
       setTimeout(function(){
         document.querySelector('.quiz').classList.toggle('hidden')}, 500)
-    } else {
+      
+    }
+     else {
       alert('quiz Over')
     }
-    
+  }, 1000)
   }
   
-  document.querySelector('.next').addEventListener('click', function(){nextQuestion(qindex)})
+  document.querySelector('.next').addEventListener('click', function(){
+    checkAnswer(qindex)
+    nextQuestion(qindex)})
   
   document.querySelector('.take-quiz').addEventListener('click', function(){
     document.querySelector('.intro').classList.toggle('hidden')
-      document.querySelector('.quiz').classList.toggle('hidden')
+    document.querySelector('.quiz').classList.toggle('hidden')
     nextQuestion(qindex)
   })
   
